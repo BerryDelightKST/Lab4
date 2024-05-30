939dd85b970f5e2f0c0bf77dc1de98dcac70cf32
Luke <khantsi.23@ichat.sp.edu.sg>
2024-05-30 12:01:14 +0800

    Updated Version 1.1

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -4,20 +4,29 @@ from time import sleep
 
 led.init()
 switch.init()
-def LedBlinker():
-    isOn=switch.read_slide_switch()
-    if isOn==True:
+def LedBlinker(starto):
+    if starto==1:
         led.set_output(led,True)
         sleep(0.2)
         led.set_output(led,False)
         sleep(0.2)
-    else:
+    elif starto==0:
         for x in range (50):
             led.set_output(led,True)
             sleep(0.1)
             led.set_output(led,False)
             sleep(0.1)
-        
+            print (x)
+            if x == 49:
+                return 0
+
+            
 
 while True:
-    LedBlinker()
+    starto=switch.read_slide_switch()
+    stopper=LedBlinker(starto)
+    while stopper==0:
+        led.set_output(led,False)
+        stopper=switch.read_slide_switch()
+        
+        
